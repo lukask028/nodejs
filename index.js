e5b5bbdbb398f0c5d01716963dc2285af6f41e7a
lukask028 <54475600+lukask028@users.noreply.github.com>
2020-02-08 12:13:33 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const port = 3000;
-const mysql = require('mysql');
 
-//Configurando o body parser
-app.use(bodyParser.urlencoded({ extend: true}));
-app.use(bodyParser.json());
-
-//Definindo as rotas
 const router = express.Router();
 
-//rota raiz
-router.get('/', (req, res) => res.json({ message: 'Funcionando!'}));
-app.use('/',router);
+// rota raiz
+router.get('/',
+    (req, res) => res.json({ message: 'Funcionando!' })
+);
 
-//incia o servidor
-app.listen(port);
-console.log('Servidor inciado e aguardando conexões na porta 3000')
-
+module.exports = router;
